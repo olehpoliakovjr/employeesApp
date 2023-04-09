81799ef995a54490e015d47cd34d752d6a536638
Oleh Poliakov <poliakovjr@ukr.net>
2023-04-09 18:39:34 +0200

    simply app

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,13 @@
+import "./app-info.css"
+
+const AppInfo = () => {
+    return (
+        <div className='app-info'>
+            <h1>Employee records in the company N</h1>
+            <h2>Total value of employees : </h2>
+            <h2>The preeminent will receive: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,14 @@
+
+import "./search-panel.css"
+
+const SearchPanel = () => {
+    return (
+        <input
+            className="form-control search-input"
+            type="text"
+            placeholder="Find an employee"
+        />
+    )
+}
+
+export default SearchPanel;
